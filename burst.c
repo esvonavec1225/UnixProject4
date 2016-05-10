c77ccc5abd164e7409d1d0712389bf6c6d4a003a
Eric Svonavec <esvonavec1225@gmail.com>
2016-05-10 18:28:33 -0400

    short parameter parsing

--- burst.c
+++ burst.c
@@ -16,10 +16,40 @@
 #define BUFFER_SIZE 8192
 
 int main( int argc, char *argv[] )  {
+	int c;
+	int help = 0;
+	int version = 0;
+	int lines_per_file = 500;
 	if (argc <= 1) {
 		printf("At least one parameter (filename) is required\n");
 	}
 	else {
+		opterr = 0;
+		while ((c = getopt(argc, argv, "hvl:")) != -1) {
+			switch(c) {
+				case 'h':
+					help = 1;
+					break;
+				case 'l':
+					lines_per_file = atoi(optarg);
+					break;
+				case 'v':
+					version = 1;
+					break;
+				default:
+					printf("Invalid Parameter(s)");
+			}
+		}
+
+		if (help == 1) {
+			printf("HELP!\n");
+			return 1;
+		}
+		else if (version == 1) {
+			printf("burst 1.0\nCopyright (C) 2016 The University of Akron.\nLicense GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\nThis is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.\n\nWritten by Eric Svonavec.\n");
+			return 1;
+		}
+
 		char *filename = argv[argc-1];
 
 		int opener = open(filename, O_RDONLY);
@@ -41,7 +71,6 @@ int main( int argc, char *argv[] )  {
 		strcat(new_filename,"1");
 		strcat(new_filename,new_ext);
 
-		int lines_per_file = 6;
 		int curline = 0;
 		int curfile = 0;
 		char buffer[BUFFER_SIZE];
