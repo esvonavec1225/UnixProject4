6aea6bcfcb0632d60a01a7f47a127cf9001e1288
Eric Svonavec <esvonavec1225@gmail.com>
2016-05-09 20:31:21 -0400

    Create first filename for burst file

--- burst.c
+++ burst.c
@@ -8,7 +8,12 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUFFER_SIZE 8192
 
 int main( int argc, char *argv[] )  {
 	if (argc <= 1) {
@@ -17,11 +22,47 @@ int main( int argc, char *argv[] )  {
 	else {
 		char *filename = argv[argc-1];
 
-		int open_int = open(filename, O_RDONLY);
-		if (open_int == -1) {
+		int opener = open(filename, O_RDONLY);
+		if (opener == -1) {
 			printf("Unable to open file: %s\n", filename);
 			return 1;
 		}
+
+		char *new_filename = malloc(strlen(filename) + 1);
+		char *dot;
+		char *ext = strrchr(filename, '.');
+		strcpy(new_filename, filename);
+		dot = strrchr(new_filename, '.');
+		*dot = '\0';
+		char new_ext[100];		
+		strcpy(new_ext, ext);
+		char *original_new_filename = new_filename;
+		strcat(new_filename,"1");
+		strcat(new_filename,new_ext);
+
+		int writer = open(new_filename, O_WRONLY | O_CREAT, 0644);
+		if (writer == -1) {
+			printf("Unable to create file: %s\n", filename);
+			return 1;
+		}
+	
+		int lines_per_file = 500;
+		int curline = 0;
+		char buffer[BUFFER_SIZE];
+		ssize_t reader;
+		while ((reader = read(opener, &buffer, BUFFER_SIZE)) > 0) {
+			for (int i = 0; i < reader; ++i) {
+				
+				printf("%c", buffer[i]);
+				if (buffer[i]=='\n') {
+					curline++;
+					if (curline == lines_per_file) {
+						curline = 0;
+					}
+				}
+			}
+		}
+		printf("Lines: %d\n", curline);
 	}
 	return 1;
 }
